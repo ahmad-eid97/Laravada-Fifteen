9cfde0921ad256025ee095583bff61643361ac91
civil.ahmad.eid <civil.ahmad.eid@gmail.com>
2023-01-29 14:15:20 +0200

    fix footer

--- components/footer/AppFooter.vue
+++ components/footer/AppFooter.vue
@@ -4,26 +4,34 @@
     <div class="d-flex justify-content-around pt-5 responsive-footer">
       <div class="w-30 mt-2 w-control">
         <img
-          src="/assets/images/logo.png"
+          :src="
+            $store.state.websiteSettings.find((one) => one.key === 'logo')
+              .plain_value
+          "
           alt="logoImage"
           style="width: 250px"
         />
         <p class="text-light my-2 fs-5">
-          Nam id sem quis mauris porttitor conse quat id vitae dolor – mauris
-          lorem ipsum nulla it amet ultricies mi dolor at pharetra.
+          {{
+            $store.state.websiteSettings.find(
+              (one) => one.key === "description"
+            ).plain_value
+          }}
         </p>
         <button class="btn test-btn mt-3">Get in touch!</button>
       </div>
 
       <div class="text-light w-30 mt-2 w-control">
         <h5 class="fw-bold mb-4">Contact info</h5>
-        <p class="my-3 footer-text">
-          Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus.
-        </p>
 
         <p class="">Phone number</p>
-        <p class="footer-text">+001 234 56 78</p>
+        <p class="footer-text">
+          {{
+            $store.state.websiteSettings.find(
+              (one) => one.key === "contact_phone"
+            ).plain_value
+          }}
+        </p>
 
         <p class="">Email</p>
         <p class="footer-text">
@@ -34,7 +42,13 @@
         </p>
 
         <p class="">Address</p>
-        <p class="footer-text">Highland St. Tonawanda, NY 14150</p>
+        <p class="footer-text">
+          {{
+            $store.state.websiteSettings.find(
+              (one) => one.key === "contact_address"
+            ).plain_value
+          }}
+        </p>
 
         <div class="d-flex d-none d-lg-flex d-md-none">
           <div class="footer-icon fs-12">
@@ -50,81 +64,77 @@
       </div>
 
       <div class="w-30 mt-2 w-control">
-        <h5 class="text-light fw-bold mb-4">Recent posts</h5>
-
-        <div>
-          <div class="d-flex">
-            <img
-              class="footer-article-img"
-              src="/assets/images/im001.jpg"
-              alt=""
-            />
-            <div class="ms-2">
-              <p class="text-light my-0">
-                Glavrida sit amet – post with side images
-              </p>
-              <p class="footer-text my-0">16th November 2018</p>
-            </div>
-          </div>
-          <div class="d-flex mt-4">
-            <img
-              class="footer-article-img"
-              src="/assets/images/im001.jpg"
-              alt=""
-            />
-            <div class="ms-2">
-              <p class="text-light my-0">
-                Glavrida sit amet – post with side images
-              </p>
-              <p class="footer-text my-0">16th November 2018</p>
-            </div>
-          </div>
-          <div class="d-flex mt-4">
-            <img
-              class="footer-article-img"
-              src="/assets/images/im001.jpg"
-              alt=""
-            />
-            <div class="ms-2">
-              <p class="text-light my-0">
-                Glavrida sit amet – post with side images
-              </p>
-              <p class="footer-text my-0">16th November 2018</p>
-            </div>
-          </div>
-          <div class="d-flex mt-4">
-            <img
-              class="footer-article-img"
-              src="/assets/images/im001.jpg"
-              alt=""
-            />
-            <div class="ms-2">
-              <p class="text-light my-0">
-                Glavrida sit amet – post with side images
-              </p>
-              <p class="footer-text my-0">16th November 2018</p>
-            </div>
+        <div class="footer-item">
+          <div class="footer-title">
+            <h5>Other Pages</h5>
+            <ul class="footer-list">
+              <!-- <li><a href="#">About Us</a></li> -->
+              <li
+                v-for="page in $store.state.footerPages"
+                :key="page.id"
+                :class="!page.status ? 'notFound' : ''"
+              >
+                <nuxt-link
+                  :to="localePath(generatePagePath(page.id))"
+                  v-if="page.status"
+                  >{{ page.name }}</nuxt-link
+                >
+              </li>
+            </ul>
           </div>
         </div>
       </div>
     </div>
 
     <div class="footer-border footer-text">
-      <img src="/assets/images/l-b-s.webp" alt="" />
       <p>© Powered By: Rail Coder - 2018-2019. All rights reserved.</p>
     </div>
   </section>
 </template>
 
 <script>
-import Subscribe from '../subscribe/subscribe.vue';
+import Subscribe from "../subscribe/subscribe.vue";
 export default {
   name: "AppFooter",
-  components: {Subscribe},
+  components: { Subscribe },
   data() {
     return {};
   },
   beforeMount() {},
-  methods: {},
+  methods: {
+    generatePagePath(id) {
+      switch (id) {
+        case 1:
+          return "/about";
+        case 2:
+          return "/contact";
+        case 3:
+          return "/terms";
+        case 4:
+          return "/policy";
+      }
+    },
+  },
 };
 </script>
+
+<style lang="scss">
+.footer-item {
+  h5 {
+    color: #fff;
+  }
+  .footer-list {
+    padding: 0;
+    list-style: none;
+    li {
+      padding: 7px 0;
+      &.notFound {
+        padding: 0;
+      }
+      a {
+        color: #fff;
+      }
+    }
+  }
+}
+</style>
